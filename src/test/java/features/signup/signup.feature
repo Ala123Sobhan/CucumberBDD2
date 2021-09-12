5a00cf2cf69445c76db1ad43e77a4e477d9fce64
Ala123Sobhan <ala123sobhan@gmail.com>
2021-09-12 18:30:28 -0400

    adding stepdef

--- src/test/java/features/signup/signup.feature
+++ src/test/java/features/signup/signup.feature
@@ -6,10 +6,10 @@ Feature: Talentek Sign up feature test suite
     And I click on  Create New Account button
     And I enter student first name, last name, valid email address, password and confirm password
     And I enter student birth month, date and year
-#    And I enter student gender as female
-#    And I agree with the terms and data use policy
-#    When I click on the Create my account
-#    Then I should be able to get the student id
+    And I enter student gender as female
+    And I agree with the terms and data use policy
+    When I click on the Create my account
+    Then I should be able to get the student id
 
 #    @signup @regression
 #  Scenario: User should not be able to sign up with invalid email address
--- src/test/java/stepdef/talteksignup.java
+++ src/test/java/stepdef/talteksignup.java
@@ -3,6 +3,8 @@ package stepdef;
 import base.Config;
 import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pageObject.SignUpPageObject;
 
 public class talteksignup extends Config {
@@ -16,13 +18,7 @@ public class talteksignup extends Config {
         su.enterLastName(fk.name().lastName());
         su.enterEmail(fk.internet().safeEmailAddress());
         su.enterPassword(fk.internet().password());
-        su.selectMonth();
-        su.selectDay();
-        su.selectYear();
-        su.selectRadioBtn("Female");
-        su.selectCheckBox();
-        su.clickCreateAccBtn();
-        su.verifySuccessMsg();
+
 
 
 
@@ -32,5 +28,33 @@ public class talteksignup extends Config {
 
     @And("I enter student birth month, date and year")
     public void iEnterStudentBirthMonthDateAndYear() {
+        su.selectMonth();
+        su.selectDay();
+        su.selectYear();
     }
+
+    @And("I enter student gender as female")
+    public void iEnterStudentGenderAsFemale() {
+        su.selectRadioBtn("Female");
+    }
+
+    @And("I agree with the terms and data use policy")
+    public void iAgreeWithTheTermsAndDataUsePolicy() {
+
+        su.selectCheckBox();
+    }
+
+    @When("I click on the Create my account")
+    public void iClickOnTheCreateMyAccount() {
+
+        su.clickCreateAccBtn();
+    }
+
+    @Then("I should be able to get the student id")
+    public void iShouldBeAbleToGetTheStudentId() {
+
+        su.verifySuccessMsg();
+
+    }
+
 }
